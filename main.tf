a89cebe7b74e6772527a7a5f310d93e74bd2d30b
linda-silva <lindasmetrics@gmail.com>
2025-01-05 17:28:13 -0700

    Ingress typo fix

--- main.tf
+++ main.tf
@@ -36,8 +36,8 @@ module "blog_sg" {
 
   vpc_id = data.aws_vpc.default.id
 
-  ingres_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingres_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
