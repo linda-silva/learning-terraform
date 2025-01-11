6f46d9f16ca709b202906070e370bc46443dbad6
linda-silva <lindasmetrics@gmail.com>
2025-01-11 14:26:55 -0700

    Allowing LB to be deleted

--- main.tf
+++ main.tf
@@ -57,10 +57,11 @@ module "autoscaling" {
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  name                       = "blog-alb"
+  vpc_id                     = module.blog_vpc.vpc_id
+  subnets                    = module.blog_vpc.public_subnets
+  security_groups            = [module.blog_sg.security_group_id]
+  enable_deletion_protection = false
 
   # Security Group
   security_group_ingress_rules = {
